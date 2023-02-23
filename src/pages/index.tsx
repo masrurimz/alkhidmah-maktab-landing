24afba54074e483604d1e8d39bac35b3aec9ce8a
Muhammad Zahid Masruri <masruri03@gmail.com>
2023-02-23 07:35:23 +0700

    Restructure folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import Landing from "~/components/home/Landing";
+import Landing from "~/features/home/Landing";
 
 const Home: NextPage = () => {
   return <Landing />;
