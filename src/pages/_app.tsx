cf73c3893ddb7756439cb33d6ac8902559bc5b29
Muhammad Zahid Masruri <masruri03@gmail.com>
2023-02-25 06:39:11 +0700

    Add main navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,12 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import MaktabHelp from "~/features/maktab/MaktabHelp/MaktabHelp";
 import MaktabMaps from "~/features/maktab/MaktabMaps/MaktabMaps";
+import MainHeader from "~/common/components/navigation/MainHeader";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <MainHeader />
       <Component {...pageProps} />
       <MaktabHelp />
       <MaktabMaps />
